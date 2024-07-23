1cc0c99137bb2a1db21d574ba1b0ed1e21ba5bb8
chakotha <ewhyte@gmail.com>
2024-07-23 20:32:59 +0100

    Finish Chap 55. Using the Submitted Data

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { NewTaskComponent } from './new-task/new-task.component';
+import { type NewTaskData } from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
@@ -59,5 +60,16 @@ export class TasksComponent {
     this.isAddingTask = false;
   }
 
-
-}
+  onAddTask(taskData: NewTaskData) {
+    // unshift adds to start of array instead of push
+    this.tasks.unshift({ 
+      id: new Date().getTime().toString(),
+      userId: this.userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate:taskData.date
+    })
+    this.isAddingTask = false;
+  }
+  
+}  
